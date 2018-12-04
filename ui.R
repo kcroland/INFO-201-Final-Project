328073d051ba152587ea99b40c7602d5bb90f1c4
Michelle Lee <mlee18@uw.edu>
2018-12-04 03:14:58 -0800

    finish plot(s), title todo

--- ui.R
+++ ui.R
@@ -7,8 +7,12 @@ my_ui <- fluidPage(
   titlePanel("Title"),  
   sidebarLayout(   # layout the page in two columns
     sidebarPanel(  # specify content for the "sidebar" column
+      uiOutput('state_selector_xy'),
+      uiOutput('x_xy_selector'),
+      uiOutput('y_xy_selector')
     ),
     mainPanel(     # specify content for the "main" column
+      plotOutput('plot_xy')
     )
   )
 )
